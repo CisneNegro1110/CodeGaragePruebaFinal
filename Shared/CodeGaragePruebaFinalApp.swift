96e39f0342fad1c2f8f8ae652a5118a04d0a1d94
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-05-20 13:23:07 -0600

    Initial Commit

---
+++ Shared/CodeGaragePruebaFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeGaragePruebaFinalApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CodeGaragePruebaFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
